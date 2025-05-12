11decfc84bd688cd8e9ff9af7f7835e5b49b787e
MirkoDD <mirkodidio111@gmail.com>
2025-05-12 14:55:41 +0200

    hello_word_text

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>laravel-primi-passi</title>
+</head>
+<body>
+    <h1>hello word</h1>
+</body>
+</html>
